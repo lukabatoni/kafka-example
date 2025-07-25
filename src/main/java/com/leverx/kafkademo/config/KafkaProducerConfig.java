30e011d565304ebda7409896aaeef22efcd1e5be
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-25 14:50:53 +0400

    add KafkaTemplate bean configuration to KafkaProducerConfig class

--- src/main/java/com/leverx/kafkademo/config/KafkaProducerConfig.java
+++ src/main/java/com/leverx/kafkademo/config/KafkaProducerConfig.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
@@ -23,9 +25,16 @@ public class KafkaProducerConfig {
     return props;
   }
 
+  @Bean
   public ProducerFactory<String, String> producerFactory(){
     return new DefaultKafkaProducerFactory<>(producerConfigs());
+  }
 
+  @Bean
+  public KafkaTemplate<String, String> kafkaTemplate(
+      ProducerFactory<String, String> producerFactory
+  ) {
+    return new KafkaTemplate<>(producerFactory);
   }
 
 }
