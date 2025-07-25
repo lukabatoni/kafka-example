b827387d54a2c1c4059a3dfbc556cb7b863004be
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-25 14:47:09 +0400

    add producer configuration method to KafkaProducerConfig class

--- src/main/java/com/leverx/kafkademo/config/KafkaProducerConfig.java
+++ src/main/java/com/leverx/kafkademo/config/KafkaProducerConfig.java
@@ -1,8 +1,25 @@
 package com.leverx.kafkademo.config;
 
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaProducerConfig {
+
+  @Value("${spring.kafka.bootstrap-servers}")
+  private String bootstrapServers;
+
+  public Map<String, Object> producerConfigs() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    return props;
+  }
+
 
 }
