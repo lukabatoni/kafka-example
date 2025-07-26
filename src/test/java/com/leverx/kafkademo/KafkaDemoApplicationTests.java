d59de63d066d3197e19cec97bc8ee4a763321009
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-26 21:17:53 +0400

    add mock KafkaTemplate for testing in KafkaDemoApplicationTests

--- src/test/java/com/leverx/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/com/leverx/kafkademo/KafkaDemoApplicationTests.java
@@ -1,10 +1,21 @@
 package com.leverx.kafkademo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootTest
 class KafkaDemoApplicationTests {
+
+  @Mock
+  private KafkaTemplate<String, String> kafkaTemplate;
+
+  @Autowired
+  void setKafkaTemplate(KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
 
   @Test
   void contextLoads() {
