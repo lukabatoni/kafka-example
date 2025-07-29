0995829ce400c0c1300a50f186bd470cf704f46d
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-29 15:52:21 +0400

    update message sending logic in CommandLineRunner to send multiple messages

--- src/main/java/com/leverx/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/leverx/kafkademo/KafkaDemoApplication.java
@@ -15,7 +15,11 @@ public class KafkaDemoApplication {
 
   @Bean
   CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
-    return args -> kafkaTemplate.send("leverX-kafka-topic", "Hello, Kafka :)");
+    return args -> {
+      for (int i = 0; i < 10; i++) {
+        kafkaTemplate.send("leverX-kafka-topic", "Message " + i);
+      }
+    };
   }
 
 }
