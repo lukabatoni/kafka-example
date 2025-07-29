c22c0014e249f2bdcfdf415af70d1fd4fc079b94
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-29 15:59:22 +0400

    add KafkaListenerContainerFactory bean to KafkaConsumerConfig for concurrent message processing

--- src/main/java/com/leverx/kafkademo/config/KafkaConsumerConfig.java
+++ src/main/java/com/leverx/kafkademo/config/KafkaConsumerConfig.java
@@ -7,8 +7,11 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 @Configuration
 public class KafkaConsumerConfig {
@@ -26,5 +29,14 @@ public class KafkaConsumerConfig {
   @Bean
   public ConsumerFactory<String, String> consumerFactory(){
     return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+  }
+
+  @Bean
+  public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> factory(
+      ConsumerFactory<String, String> consumerFactory
+  ){
+    ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory);
+    return factory;
   }
 }
