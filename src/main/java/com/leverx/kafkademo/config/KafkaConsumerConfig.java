33c5c0a604ab1096bfc2055382948dc62564a698
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-29 15:54:03 +0400

    add KafkaConsumerConfig class for consumer configuration

---
+++ src/main/java/com/leverx/kafkademo/config/KafkaConsumerConfig.java
@@ -0,0 +1,22 @@
+package com.leverx.kafkademo.config;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConsumerConfig {
+  @Value("${spring.kafka.bootstrap-servers}")
+  private String bootstrapServers;
+
+  public Map<String, Object> consumerConfigs() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    return props;
+  }
+}
