0dc5fb6d8a7bad37c457425f69f4460a12b60c8a
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-30 15:08:30 +0400

    add constructor to MessageController for KafkaTemplate injection

--- src/main/java/com/leverx/kafkademo/MessageController.java
+++ src/main/java/com/leverx/kafkademo/MessageController.java
@@ -1,6 +1,7 @@
 package com.leverx.kafkademo;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class MessageController {
 
   private KafkaTemplate<String, String> kafkaTemplate;
 
+  public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  @PostMapping
   public void publish(@RequestBody MessageRequest request){
     kafkaTemplate.send("leverX-kafka-topic", request.toString());
   }
