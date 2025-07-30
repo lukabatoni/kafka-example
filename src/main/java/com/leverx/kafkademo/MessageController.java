336b8a6dc053b43ae164629e06b15474c022c652
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-30 15:07:39 +0400

    add KafkaTemplate to MessageController for publishing messages to Kafka topic

--- src/main/java/com/leverx/kafkademo/MessageController.java
+++ src/main/java/com/leverx/kafkademo/MessageController.java
@@ -1,5 +1,6 @@
 package com.leverx.kafkademo;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/messages")
 public class MessageController {
-  public void publish(@RequestBody MessageRequest request){
 
+  private KafkaTemplate<String, String> kafkaTemplate;
+
+  public void publish(@RequestBody MessageRequest request){
+    kafkaTemplate.send("leverX-kafka-topic", request.toString());
   }
 }
