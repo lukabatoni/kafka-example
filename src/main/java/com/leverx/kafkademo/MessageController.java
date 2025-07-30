1dad3b08f8aef180a6ad6fc8046a4d7485c196ac
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-30 15:06:15 +0400

    add publish method to MessageController for handling message publishing

--- src/main/java/com/leverx/kafkademo/MessageController.java
+++ src/main/java/com/leverx/kafkademo/MessageController.java
@@ -1,4 +1,13 @@
 package com.leverx.kafkademo;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/messages")
 public class MessageController {
+  public void publish(@RequestBody MessageRequest request){
+
+  }
 }
