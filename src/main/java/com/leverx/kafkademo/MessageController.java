5c856df358ccefc8215bd9407520ca9f3e9dde37
LukaOniani <lukaa.oniani2002@gmail.com>
2025-07-31 19:11:30 +0400

    make kafkaTemplate field final in MessageController for immutability

--- src/main/java/com/leverx/kafkademo/MessageController.java
+++ src/main/java/com/leverx/kafkademo/MessageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/messages")
 public class MessageController {
 
-  private KafkaTemplate<String, String> kafkaTemplate;
+  private final KafkaTemplate<String, String> kafkaTemplate;
 
   public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
     this.kafkaTemplate = kafkaTemplate;
